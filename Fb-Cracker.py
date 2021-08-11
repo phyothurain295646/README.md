63dfee06f96ad5984a903e06be3988a38cc6d5ca
ۥٰ ۥٰ <88605415+phyothurain295646@users.noreply.github.com>
2021-08-11 18:34:41 +0630

    Fb-Cracker.py
    pkg update
    
    pkg update
    
    pkg install git
    
    pkg install git
    
    pkg install python
    
    pkg install python2
    
    https://github.com/phyothurain295646/README.md/blob/main/README.md
    
    cd fb-cracker
    
     pip install -r requirments.txt
    
    HOW TO USE:
    
    
    python2 fb-phyothurain.py

---
+++ Fb-Cracker.py
@@ -0,0 +1,26 @@
+pkg update
+
+pkg update
+
+pkg install git
+
+pkg install git
+
+pkg install python
+
+pkg install python2
+
+https://github.com/phyothurain295646/README.md/blob/main/README.md
+
+cd fb-cracker
+
+ pip install -r requirments.txt
+
+HOW TO USE:
+
+
+python2 fb-phyothurain.py
+
+
+
+
